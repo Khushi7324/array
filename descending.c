2f2d88f12d0a5eae9ed718c5b78d0774ef0eb0f8
Khushi7324 <khushiyadav0734@gmail.com>
2025-01-16 14:33:17 +0530

    Add files via upload

---
+++ descending.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int arr[10]={90,40,60,80,10,30,50,99,87,56};
+	int i,j,k;
+	for(i=0;i<10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if(arr[i]<arr[j])
+			{
+			k=arr[i];
+			arr[i]=arr[j];
+			arr[j]=k;
+		}
+		}
+	}
+printf("Array:");
+for(i=0;i<10;i++)
+{
+printf("%d ",arr[i]);
+}
+}
+
